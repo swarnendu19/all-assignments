3f240d00caeed49fa22493ca2daec46707da21e5
Swarnendu <swarnendumaity633@gmail.com>
2023-12-21 16:37:32 +0530

    Assignment

--- week-3/02-course-app-easy/index.js
+++ week-3/02-course-app-easy/index.js
@@ -7,17 +7,42 @@ let ADMINS = [];
 let USERS = [];
 let COURSES = [];
 
+function  adminAuthentication(req,res,next){
+  // const username=req.headers.username
+  // const password =req.headers.password
+  const{username,password}=req.headers // The above two lines are same as this lines
+  const admin=ADMINS.find(a => a.username===username && a.password===password)
+  if(admin){
+    next()
+  }
+  else{
+    res.status(404)
+  }
+}
+
 // Admin routes
 app.post('/admin/signup', (req, res) => {
   // logic to sign up admin
+  const admin = req.body
+  const existingAdmin=ADMINS.find(a => a.username===admin.username)
+  if(existingAdmin){
+    res.status(403).json({message: 'Already Account created'})
+
+  }
+  else{
+    ADMINS.push(admin)
+    res.json({message: 'Logged in sucessfully'})
+  }
 });
 
-app.post('/admin/login', (req, res) => {
+app.post('/admin/login', adminAuthentication,(req, res) => {
   // logic to log in admin
+    res.json({message: 'logged in sucessfully', courseId : COURSES.id})
 });
 
 app.post('/admin/courses', (req, res) => {
   // logic to create a course
+
 });
 
 app.put('/admin/courses/:courseId', (req, res) => {
