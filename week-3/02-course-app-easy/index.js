90955806ee3d1c61b4987d78ce8f8060b95b7db1
Swarnendu <swarnendumaity633@gmail.com>
2023-12-23 21:24:38 +0530

    Assignment

--- week-3/02-course-app-easy/index.js
+++ week-3/02-course-app-easy/index.js
@@ -27,7 +27,7 @@ app.post('/admin/signup', (req, res) => {
   const existingAdmin=ADMINS.find(a => a.username===admin.username)
   if(existingAdmin){
     res.status(403).json({message: 'Already Account created'})
-
+  
   }
   else{
     ADMINS.push(admin)
@@ -37,37 +37,71 @@ app.post('/admin/signup', (req, res) => {
 
 app.post('/admin/login', adminAuthentication,(req, res) => {
   // logic to log in admin
-    res.json({message: 'logged in sucessfully', courseId : COURSES.id})
+    res.json({message: 'logged in sucessfully'})
 });
 
-app.post('/admin/courses', (req, res) => {
+app.post('/admin/courses',adminAuthentication, (req, res) => {
   // logic to create a course
+  const course = req.body
+  course.id=Date.now()
+  COURSES.push(course)
+  res.json({message: 'Course Created sucessfully',courseId: course.id})
 
 });
 
-app.put('/admin/courses/:courseId', (req, res) => {
+app.put('/admin/courses/:courseId', adminAuthentication,(req, res) => {
   // logic to edit a course
+  const courseId = parseInt(req.params.courseId)
+  const course = COURSES.find(c => c.id=== courseId)
+  if(course){
+    Object.assign(course,req.body)
+    res.json({message: 'Course updated sucessfully'})
+  }
+  else{
+    res.status(404).json({message: 'Course not found'})
+  }
 });
 
-app.get('/admin/courses', (req, res) => {
+app.get('/admin/courses',adminAuthentication, (req, res) => {
   // logic to get all courses
+  res.json({courses : COURSES})
 });
 
 // User routes
 app.post('/users/signup', (req, res) => {
   // logic to sign up user
+  const newUser= {...req.body,purchasedCourse: []}
+   USERS.push(newUser)
+   res.json({message: 'User created sucessfully'})
 });
 
-app.post('/users/login', (req, res) => {
+app.post('/users/login',userAuthentication, (req, res) => {
   // logic to log in user
+  res.json({message: 'logged in Sucessfully'})
 });
 
 app.get('/users/courses', (req, res) => {
   // logic to list all courses
+  let filteredCourses=[];
+  for(let i=0;i<COURSES.length;i++){
+    if(COURSES[i].published){
+    filteredCourses.push(COURSES[i])
+  }
+}
+res.json({courses : filteredCourses})
 });
 
 app.post('/users/courses/:courseId', (req, res) => {
   // logic to purchase a course
+  const courseId=Number(req.params.courseId)
+  const course= COURSES.find(c => c.id === courseId && c.published)
+  if(course){
+    req.user.purchasedCourse.push(courseId)
+    res.json({message: 'Course Purchased sucessfully'})
+  }
+  else{
+    res.status(404).json({message: 'Course not found '})
+  }
 });
 
 app.get('/users/purchasedCourses', (req, res) => {
